ccb83dda09c89c3989258d0ac322d097387a3683
feindreh <119032218+feindreh@users.noreply.github.com>
2022-12-12 10:56:57 +0100

    add delete button

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ function makeRow(){
 
 }
 
-function makeGrid(){
-    squares = prompt("enter Rows length");
+function makeGrid(a){
+    if(a=="yes"){getSquares()};
     deleteGrid();
     for(let i = 0; i < squares; i++){
         makeRow()
@@ -37,11 +37,17 @@ function deleteGrid(){
     container.textContent = ""
 }
 
+function getSquares(){
+    squares = prompt("enter Rows length");
+}
 
-const container = document.querySelector("#container")
-const button = document.querySelector("#button")
 
-button.addEventListener('click', () => makeGrid())
+const container = document.querySelector("#container");
+const button1 = document.querySelector("#button");
+const button2 = document.querySelector("#delete");
+
+button1.addEventListener('click', () => makeGrid("yes"))
+button2.addEventListener('click', () => makeGrid("no"))
 
 let squares = 0;
 
